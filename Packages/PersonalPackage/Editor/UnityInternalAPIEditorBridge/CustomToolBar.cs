52ff738070460e33b46a2339cab1b8bd6a673da7
ちんぱん <52692512+chinpanGX@users.noreply.github.com>
2024-09-20 22:21:01 +0900

    プロジェクトのパス名とブランチ名をツールバーに表示する

--- Packages/PersonalPackage/Editor/UnityInternalAPIEditorBridge/CustomToolBar.cs
+++ Packages/PersonalPackage/Editor/UnityInternalAPIEditorBridge/CustomToolBar.cs
@@ -1,39 +1,87 @@
 using System.IO;
-using PlasticPipe.PlasticProtocol.Messages;
 using UnityEditor;
-using UnityEditor.PackageManager.UI.Internal;
 using UnityEngine;
 using UnityEngine.UIElements;
 
-[InitializeOnLoad]
-public class CustomToolBar
+namespace PersonalPackage
 {
-    [InitializeOnLoadMethod]
-    public static void Execute()
-    { 
-        EditorApplication.update += OnUpdate;
-    }
-
-    private static void OnUpdate()
+    public static class CustomToolBar
     {
-        var toolbar = Toolbar.get;
-        if (toolbar.windowBackend?.visualTree is not VisualElement visualTree) return; // ツールバーのVisualTreeを取得
-        if (visualTree.Q("ToolbarZoneRightAlign") is not { } rightZone) return; // ツールバー左側のゾーンを取得
-        rightZone.Add(new Label($"{GetProjectPath()}"));
-        
-        EditorApplication.update -= OnUpdate;
-    }
+        private static readonly string CurrentPathName = "";
+        private static string currentBranchName = "";
+        private static Label label;
 
-    private static string GetProjectPath()
-    {
-        var assetsDirInfo = new DirectoryInfo(Application.dataPath);
-        var projectPathDirInfo = assetsDirInfo.Parent;
-        var projectPathParent = projectPathDirInfo?.Parent;
-        return $"パス: {projectPathParent?.Name}/{projectPathDirInfo?.Name}";
-    }
+        [InitializeOnLoadMethod]
+        private static void InitializeOnLoad()
+        {
+            EditorApplication.update += Setup;
+            EditorApplication.focusChanged += (isFocus) =>
+            {
+                if (isFocus)
+                {
+                    Execute();
+                }
+            };
+        }
 
-    private static string GetBranchName()
-    {
-        return "";
+        static CustomToolBar()
+        {
+            CurrentPathName = GetProjectPath(new DirectoryInfo(Application.dataPath));
+        }
+
+        private static void Setup()
+        {
+            var toolbar = Toolbar.get;
+            if (toolbar.windowBackend?.visualTree is not VisualElement visualTree) return; // ツールバーのVisualTreeを取得
+            if (visualTree.Q("ToolbarZoneRightAlign") is not { } rightZone) return; // ツールバー左側のゾーンを取得
+
+            var assets = new DirectoryInfo(Application.dataPath);
+            var branchName = GetBranchName(assets);
+            currentBranchName = branchName;
+
+            label = new Label($"パス名: {CurrentPathName} / ブランチ名: {currentBranchName}");
+            rightZone.Add(label);
+            EditorApplication.update -= Setup;
+        }
+
+        private static void Execute()
+        {
+            var assets = new DirectoryInfo(Application.dataPath);
+            var branchName = GetBranchName(assets);
+            if (branchName != null && currentBranchName == branchName)
+                return;
+
+            currentBranchName = branchName;
+            label.text = $"パス名: {CurrentPathName} / ブランチ名: {currentBranchName}";
+        }
+
+        private static string GetProjectPath(DirectoryInfo assetsFolder)
+        {
+            var projectPathDirInfo = assetsFolder.Parent;
+            var projectPathParent = projectPathDirInfo?.Parent;
+            return $"{projectPathParent?.Name}/{projectPathDirInfo?.Name}";
+        }
+
+        private static string SearchGitFolder(DirectoryInfo dirInfo)
+        {
+            var parent = dirInfo.Parent;
+            if (parent == null)
+                return "";
+
+            var searchGitPath = $"{parent.FullName.Replace("\\", "/")}/.git";
+            if (!Directory.Exists(searchGitPath))
+            {
+                return SearchGitFolder(parent);
+            }
+            return searchGitPath;
+        }
+
+        private static string GetBranchName(DirectoryInfo assets)
+        {
+            var gitPath = SearchGitFolder(assets);
+            using var reader = new StreamReader($"{gitPath}/HEAD");
+            var refs = reader.ReadLine();
+            return refs?.Substring("ref: refs/heads/".Length);
+        }
     }
 }
